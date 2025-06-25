df26d5b953d66fe61765083adf7e9b4ce8d0f79e
MOONUJ <141498537+MOONUJ@users.noreply.github.com>
2025-06-25 17:00:15 +0900

    버전 관리 시작
    Action Version: 0.0

---
+++ vra/admin/abx/Custom.AIP.attach.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+'''
+@copyright: Equal Plus
+@author: Hye-Churn Jang
+'''
+
+#===============================================================================
+# Import Libraries Here
+#===============================================================================
+import json
+import time
+
+
+#===============================================================================
+# AaManager SDK
+#===============================================================================
+class AaManager:
+    def __init__(self, context): self.context = context
+    def toJson(self, response):
+        if response['status'] >= 400: raise Exception(response['content'].decode('utf-8'))
+        try: return json.loads(response['content'].decode('utf-8'))
+        except: return response['content'].decode('utf-8')
+    def encode(self, url): return url.replace(' ', '%20').replace('$', '%24').replace("'", '%27').replace('[', '%5B').replace(']', '%5D')
+    def get(self, url): return self.toJson(self.context.request(operation='GET', link=self.encode(url), body=''))
+    def post(self, url, data): return self.toJson(self.context.request(operation='POST', link=self.encode(url), body=data))
+    def put(self, url, data): return self.toJson(self.context.request(operation='PUT', link=self.encode(url), body=data))
+    def patch(self, url, data): return self.toJson(self.context.request(operation='PATCH', link=self.encode(url), body=data))
+    def delete(self, url, data=''): return self.toJson(self.context.request(operation='DELETE', link=self.encode(url), body=data))
+    def getUerp(self, url): return self.get(f'/provisioning/uerp{url}')
+    def postUerp(self, url, data): return self.post(f'/provisioning/uerp{url}', data)
+    def putUerp(self, url, data): return self.put(f'/provisioning/uerp{url}', data)
+    def patchUerp(self, url, data): return self.patch(f'/provisioning/uerp{url}', data)
+    def deleteUerp(self, url, data=''): return self.delete(f'/provisioning/uerp{url}', data)
+    def runOrchAction(self, projectId, uri, data={}):
+        result = self.post(f'/form-service/api/forms/renderer/external-value?projectId={projectId}', {'uri':uri, 'dataSource':'scriptAction', 'parameters':[{'name':k, 'value':v} for k, v in data.items()]})
+        if 'data' in result: return result['data']
+        elif 'error' in result: raise Exception(result['error']['summaryMessage'])
+        else: raise Exception('unknown error')
+
+
+#===============================================================================
+# Implement Handler Here
+# ------------------------------------------------------------------------------
+# Default : computeLink :
+# Default : interfaceLink :
+#===============================================================================
+def handler(context, inputs):
+    if inputs['compute'] != 'NONE': raise Exception('Access IP already has allocated another machine')
+    aa = AaManager(context)
+
+    projectId = inputs['__metadata']['project']
+    accessIpId = inputs['id']
+    computeLink = inputs['computeLink']
+    compute = aa.getUerp(computeLink);
+    interfaceLink = inputs['interfaceLink']
+    for aip in aa.get(f'/deployment/api/resources?resourceTypes=Custom.AIP&projects={projectId}')['content']:
+        if aip['properties']['id'] == accessIpId:
+            resourceId = aip['id']
+            break
+    else: raise Exception('could not find current resource id')
+    interfaceAddress = aa.runOrchAction(projectId, 'com.gvp.bp.aip/attachAccessIp', { #원래 bvp 였음
+        'interfaceLink': interfaceLink,
+        'accessIpId': accessIpId,
+        'destinationIp': inputs['address']
+        #'resourceId': resourceId
+    })
+
+    inputs['compute'] = computeLink
+    inputs['computeName'] = compute['name']
+    inputs['interface'] = interfaceLink
+    inputs['interfaceAddress'] = interfaceAddress
+    outputs = inputs
+    outputs.pop("computeLink")
+    outputs.pop('interfaceLink')
+    return outputs
