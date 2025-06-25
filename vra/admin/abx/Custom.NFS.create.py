3c6ff184f005ae64fdcc6f2937eca704273aa86a
MOONUJ <141498537+MOONUJ@users.noreply.github.com>
2025-06-25 17:01:43 +0900

    버전 관리 시작
    Action Version: 0.0

---
+++ vra/admin/abx/Custom.NFS.create.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+'''
+@copyright: Equal Plus
+@author: Hye-Churn Jang
+'''
+
+#===============================================================================
+# Import Libraries Here
+#===============================================================================
+import json
+import time
+
+
+#===============================================================================
+# AaManager SDK
+#===============================================================================
+class AaManager:
+    def __init__(self, context): self.context = context
+    def toJson(self, response):
+        if response['status'] >= 400: raise Exception(response['content'].decode('utf-8'))
+        try: return json.loads(response['content'].decode('utf-8'))
+        except: return response['content'].decode('utf-8')
+    def encode(self, url): return url.replace(' ', '%20').replace('$', '%24').replace("'", '%27').replace('[', '%5B').replace(']', '%5D')
+    def get(self, url): return self.toJson(self.context.request(operation='GET', link=self.encode(url), body=''))
+    def post(self, url, data): return self.toJson(self.context.request(operation='POST', link=self.encode(url), body=data))
+    def put(self, url, data): return self.toJson(self.context.request(operation='PUT', link=self.encode(url), body=data))
+    def patch(self, url, data): return self.toJson(self.context.request(operation='PATCH', link=self.encode(url), body=data))
+    def delete(self, url, data=''): return self.toJson(self.context.request(operation='DELETE', link=self.encode(url), body=data))
+    def getUerp(self, url): return self.get(f'/provisioning/uerp{url}')
+    def postUerp(self, url, data): return self.post(f'/provisioning/uerp{url}', data)
+    def putUerp(self, url, data): return self.put(f'/provisioning/uerp{url}', data)
+    def patchUerp(self, url, data): return self.patch(f'/provisioning/uerp{url}', data)
+    def deleteUerp(self, url, data=''): return self.delete(f'/provisioning/uerp{url}', data)
+    def runOrchAction(self, projectId, uri, data={}):
+        result = self.post(f'/form-service/api/forms/renderer/external-value?projectId={projectId}', {'uri':uri, 'dataSource':'scriptAction', 'parameters':[{'name':k, 'value':v} for k, v in data.items()]})
+        if 'data' in result: return result['data']
+        elif 'error' in result: raise Exception(result['error']['summaryMessage'])
+        else: raise Exception('unknown error')
+
+
+#===============================================================================
+# Implement Handler Here
+# ------------------------------------------------------------------------------
+# Default : apiVersion :  2021-07-15
+#===============================================================================
+def get_label_by_value(data: list, target_value: str):
+    """
+    주어진 리스트에서 특정 value에 해당하는 label을 반환합니다.
+    
+    :param data: [{'label': ..., 'value': ...}, ...] 형식의 리스트
+    :param target_value: 찾고자 하는 value 문자열
+    :return: 해당 value의 label, 없으면 None
+    """
+    return next((item['label'] for item in data if item['value'] == target_value), None)
+
+def handler(context, inputs):
+    aa = AaManager(context)
+    name = inputs['name']
+    size = inputs['size']
+    systemType = inputs['systemType']
+    projectId = inputs['__metadata']['project']
+    if systemType == 'ceph':
+        data = aa.runOrchAction(projectId,"com.gvp.bp.nfs/nfsProvision",{"nfsName" : name, "size": size})
+        
+        inputs['volumeName'] = get_label_by_value(data,'volumeName')
+        inputs['subvolumeName'] = get_label_by_value(data,'subvolumeName')
+        inputs['pusedo'] = get_label_by_value(data,'pusedo')
+        inputs['accessType']  = get_label_by_value(data,'accessType')
+        inputs['squash']  = get_label_by_value(data,'squash')
+        inputs['clients']  = get_label_by_value(data,'clients')
+        inputs['exportId'] = get_label_by_value(data,'exportId')
+        inputs['nfsClusterId'] = get_label_by_value(data,'nfsClusterId')
+        inputs['usage'] = get_label_by_value(data,'usage')
+    elif systemType == 'vsan':
+        data = aa.runOrchAction(projectId,"com.gvp.bp.vsanfs/fileShareCreate",{"volumeName" : name, "size": size})
+        
+        inputs['fileShareUuid'] = get_label_by_value(data,'uuid')
+        inputs['pusedo'] = get_label_by_value(data,'pusedo')
+        inputs['accessType']  = get_label_by_value(data,'accessType')
+        inputs['squash']  = get_label_by_value(data,'squash')
+        inputs['clients']  = get_label_by_value(data,'clients')
+        inputs['usage'] = get_label_by_value(data,'usage')
+        inputs['endpoint'] = get_label_by_value(data,'endpoint')
+        inputs['endpointCluster'] = get_label_by_value(data,'endpointCluster')
+    
+
+    
+
+    outputs = inputs
+    return outputs
